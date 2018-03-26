72efcb073968396839bcd4f011630ff9816bbb13
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-03-26 20:30:16 +0200

    Minor updates to music player UI

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -8,14 +8,18 @@ class PlayerBar extends Component {
     return(
       <section className="player-bar">
         <section id="buttons">
-          <button id="previous" className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored" onClick={this.props.handlePrevClick}>
-            <i className="material-icons">skip_previous</i>
+          <button id="previous" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={this.props.handlePrevClick}>
+            <i className="material-icons md-18">skip_previous</i>
           </button>
-          <button id="play-pause" className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored" onClick={this.props.handleSongClick} >
-             <i className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></i>
+          <button id="play-pause" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={this.props.handleSongClick} >
+             {
+               this.props.isPlaying ?
+               <i className="material-icons md-18">play_arrow</i> :
+               <i className="material-icons md-18">pause</i>
+             }
           </button>
-          <button id="next" className="mdl-button mdl-js-button mdl-button--fab mdl-button--colored" onClick={this.props.handleNextClick}>
-            <i className="material-icons">skip_next</i>
+          <button id="next" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={this.props.handleNextClick}>
+            <i className="material-icons md-18">skip_next</i>
           </button>
         </section>
         <section id="time-control">
@@ -27,7 +31,6 @@ class PlayerBar extends Component {
               onChange={this.props.handleTimeChange}
               />
             </div>
-
         </section>
         <section id="volume-control">
             <i className="volume-label material-icons">volume_down</i>
